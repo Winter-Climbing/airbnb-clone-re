d21123f6896ee665e32b359ca4eaf625a915b636
Gose <niabc70@gmail.com>
2022-08-30 14:00:38 +0900

    nav 캐러셀
    리액트 슬릭 이용

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,106 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
 import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import data from './NavDataTest.json'
+import styles from './Nav.module.css'
 
 
-// import 'swiper/swiper.scss';
+
 
 
 
 
 const Test = () => {
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", }}
+      onClick={onClick}
+    >
+      <div className={styles.nav_right_btnSpace}>
+        <div className={styles.nav_right_btnSpace_btn}> 
+          <img src="./img/rightBtn.svg" alt="오른쪽버튼" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block"}}
+      onClick={onClick}
+    >
+      <div className={styles.nav_left_btnSpace}>
+        <div className={styles.nav_left_btnSpace_btn}> 
+        <img src="./img/leftBtn.svg" alt="왼쪽버튼" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+  const settings = {
+  dots: false,
+  infinite: false,
+  speed: 700,
+  slidesToShow: 14,
+  slidesToScroll: 9,
+  initialSlide: 0,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+  return (
+    <>
+    <div className={styles.nav_flex}>
+      <Slider {...settings}>
+        {data.map((item) => {
+          return (
+          <button key={item.id} className={styles.nav_btn}>
+            <div className={styles.nav_btn_title}>
+              <img className={styles.nav_btn_title_img} src=  {item.img} alt={item.title} />
+              <p className={styles.nav_btn_title_p}>{item.title}</p>
+            </div>
+          </button>
+          )
+        })}
+      </Slider>
+    </div>
+    </>
+  )
 
 };
 
