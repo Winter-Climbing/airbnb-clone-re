68f2b2858c88b2aeaa298b9d0126d9a8e47fffac
Gose <niabc70@gmail.com>
2022-09-03 21:39:32 +0900

    좋아요 버튼 수정

--- src/components/HomePage/Main/Main.jsx
+++ src/components/HomePage/Main/Main.jsx
@@ -2,26 +2,42 @@ import React from 'react';
 import LodgingData from "../../../lib/LodgingData.json"
 import styles from "./Main.module.css"
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 const Main = () => {
   <Link to="/detailpage"></Link>
+  const [heartBtn, setHeartBtn] = useState(false)
+  const heartBtnPainting = () => {
+    setHeartBtn(!heartBtn)
+  }
+
 
   return (
     <>
       {
-        LodgingData.map((lodging) => {
+        LodgingData.map((lodging, index) => {
             return (
-              <div key={lodging.id}>
-                <Link to="/detailpage">
+              <div key={index}>
+
                   <section className={styles.lodging}>
                     <div className={styles.lodging_img}>
                       <img className={styles.lodging_img_objectfit} src={lodging.background} alt="몰디브사진" />
-                      <button className={styles.lodging_img_heartBtn}>
-                        <img
-                          src="./img/favorite_border.svg"
-                          alt="하트아이콘"
-                          className={styles.heartIcon}
-                        />
+                      <button className={styles.lodging_img_heartBtn} onClick={heartBtnPainting}>
+                        {
+                          heartBtn === false ?
+                          <img
+                            src="./img/favorite_border.svg"
+                            alt="하트아이콘"
+                            className={styles.heartIcon}
+                          />
+
+                          :
+                          <img
+                            src="./img/favorite_border.svg"
+                            alt="하트아이콘"
+                            className={styles.heartIconPaint}
+                          />
+                        }
                       </button>
                       <button className={styles.lodging_rightBtn}>&#62;</button>
                     </div>
@@ -37,7 +53,7 @@ const Main = () => {
                       <p><span className={styles.lodging_text_price}>&#8361;{lodging.price}</span> / 박</p>
                     </div>
                   </section>
-                </Link>
+
               </div>
             )  
           })
